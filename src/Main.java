ef3f57ffa2568bc8e16a38439aa289108f71c7c4
Анастасия <fumi_nako@mail.ru>
2022-10-26 00:13:53 +0300

    Домашнее

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       boolean a = AuthorizationData.validate("asdasd", "waw", "wawwww");
+       if (a){
+           System.out.println("Данные верны");
+       } else {
+           System.out.println("Данные не верны");
+       }
     }
 }
